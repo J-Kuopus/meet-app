bbbc87585a8336ba92224e7904d9e520f6468f05
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-10 12:30:52 +0200

    Changed to earlier version

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,16 +1,15 @@
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
-
 /**
  * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
- * update the calendar yourself. 
+ * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
  */
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 /**
  * Credentials are those values required to get access to your calendar. If you see “process.env” this means
- * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden.
+ * the value is in the “config.json” file. This is a best practice as it keeps your API secrets hidden. Please remember to add “config.json” to your “.gitignore” file.
  */
 const credentials = {
   client_id: process.env.CLIENT_ID,
@@ -23,7 +22,6 @@ const credentials = {
   redirect_uris: ["https://j-kuopus.github.io/meet-app/"],
   javascript_origins: ["https://j-kuopus.github.io", "http://localhost:3000"],
 };
-
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
@@ -38,9 +36,8 @@ const oAuth2Client = new google.auth.OAuth2(
  * as a URL parameter.
  *
  */
-
 module.exports.getAuthURL = async () => {
-   /**
+  /**
    *
    * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
    * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
@@ -50,7 +47,6 @@ module.exports.getAuthURL = async () => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
-
   });
 
   return {
@@ -64,3 +60,42 @@ module.exports.getAuthURL = async () => {
   };
 };
 
+module.exports.getAccessToken = async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+  
+    return new Promise((resolve, reject) => {
+      /**
+       *  Exchange authorization code for access token with a “callback” after the exchange,
+       *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+       */
+  
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+      .then((token) => {
+        // Respond with OAuth token 
+        return {
+          statusCode: 200,
+          body: JSON.stringify(token),
+        };
+      })
+      .catch((err) => {
+        // Handle error
+        console.error(err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify(err),
+        };
+      });
+  };
