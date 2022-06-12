aea159c595607452fb898052fb638187febee059
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-12 14:01:09 +0200

    Created new components and tests

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { mockData } from './mock-data';
 
 class Event extends Component {
+  state = {
+    collapsed: true,
+  };
+
+  handleClick = () => {
+    this.state.collapsed
+      ? this.setState({ collapsed: false })
+      : this.setState({ collapsed: true });
+  };
+
+  showSummary = () => {
+    if (this.state.collapsed === false) {
+      return mockData[0].description;
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div className="event">
+        <h3 className="title">{event.summary}</h3>
+        <p className="start-time">
+          {event.start.dateTime} {event.start.timeZone}
+        </p>
+        <p className="location">{event.location}</p>
+        <button className="btn-details" onClick={this.handleClick}>
+          show details
+        </button>
+        <div className="event-details">{this.showSummary()}</div>
+      </div>
+    );
   }
 }
 export default Event;
