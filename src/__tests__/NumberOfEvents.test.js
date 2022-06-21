e1d5e0372533cdd7d374147bfe9f595ba2afaeb9
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-21 09:40:55 +0200

    Updated test descriptions

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -8,36 +8,24 @@ describe('<NumberOfEvents /> component', () => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
 
-  test('render textbox', () => {
+  test('render number of events input', () => {
     expect(NumberOfEventsWrapper.find('#events-number')).toHaveLength(1);
   });
 
-  test('display number 32 by default', () => {
-    expect(
-      NumberOfEventsWrapper.find('#events-number').get(0).props.value
-    ).toEqual(32);
+  test('number of events displayed is 32 by default', () => {
+    expect(NumberOfEventsWrapper.find('#events-number').get(0).props.value).toEqual(32);
   });
 
-  test('user can change the number of events', () => {
-    NumberOfEventsWrapper.find('#events-number').simulate('change', {
-      target: { value: 10 },
+  test('if user enters number lower than number of available events', () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find('#events-number').simulate('change', {target: { value: -1 },
+    });
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
+  });
+
+  test('user can change the number of events to display', () => {
+    NumberOfEventsWrapper.find('#events-number').simulate('change', {target: { value: 10 },
     });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(10);
-  });
-
-  test('only allow number above 1 of events > 0', () => {
-    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
-    NumberOfEventsWrapper.find('#events-number').simulate('change', {
-      target: { value: -1 },
-    });
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
-  });
-
-  test('only allow number', () => {
-    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
-    NumberOfEventsWrapper.find('#events-number').simulate('change', {
-      target: { value: 'string' },
-    });
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
   });
 });
