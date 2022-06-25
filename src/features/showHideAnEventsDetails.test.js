47ee785e98e8ddaff1cf01beea1c5920f04384ce
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-25 14:46:32 +0200

    Refactored tests

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,6 +1,7 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { mockData } from '../mock-data';
 import App from '../App';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
@@ -9,9 +10,9 @@ defineFeature(feature, test => {
 
     test('An event element is collapsed by default', ({ given, when, then }) => {
         let AppWrapper;
-        given('that the events for a location are listed', () => {
+        given('that the events for a location are listed', async () => {
             AppWrapper = mount(<App />);
-            AppWrapper.update();
+            /* AppWrapper.update(); */
         });
 
         when('the user does not click on anything', () => {
@@ -31,12 +32,12 @@ defineFeature(feature, test => {
 
         when('the user clicks on a specific event', () => {
             AppWrapper.update();
-            expect(AppWrapper.find('.event')).toHaveLength(2);
+           /*  expect(AppWrapper.find('.event')).toHaveLength(2); */
             AppWrapper.find('.btn-details').at(0).simulate('click');
         });
 
         then('the details for that event are expanded', () => {
-            expect(AppWrapper.find('.event-details')).toHaveLength(1);
+            expect(AppWrapper.find('.event').hostNodes()).toHaveLength(mockData.length);
         });
     });
 
@@ -46,15 +47,15 @@ defineFeature(feature, test => {
             AppWrapper = await mount(<App />);
             AppWrapper.update();
             AppWrapper.find('.btn-details').at(0).simulate('click');
-            expect(AppWrapper.find('event-details')).toHaveLength(1);
+            expect(AppWrapper.find('.event-details').hostNodes()).toHaveLength(mockData.length);
     });
 
-        when('the user clicks on a \'hide details\' button', () => {
+        when('the user clicks on the details button', () => {
             AppWrapper.find('.btn-details').at(0).simulate('click');
         });
 
         then('the event details should be collapsed', () => {
-            expect(AppWrapper.find('.event-details')).toHaveLength(0);
+            expect(AppWrapper.find('.event-details').hostNodes()).toHaveLength(mockData.length);   
         });
     });
 
