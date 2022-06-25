fc34e3375f667a889f960d513b344e12dc983269
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-06-25 13:53:38 +0200

    Updated test descriptions

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -8,22 +8,22 @@ describe('<NumberOfEvents /> component', () => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
 
-  test('render number of events input', () => {
+  test('if number of events input is rendered', () => {
     expect(NumberOfEventsWrapper.find('#events-number')).toHaveLength(1);
   });
 
-  test('number of events displayed is 32 by default', () => {
+  test('if default number of events loaded is 32', () => {
     expect(NumberOfEventsWrapper.find('#events-number').get(0).props.value).toEqual(32);
   });
 
-  test('if user enters number lower than number of available events', () => {
+  test('value of numberOfEvents state does not change when inserted number is less than 1', () => {
     NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
     NumberOfEventsWrapper.find('#events-number').simulate('change', {target: { value: -1 },
     });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(32);
   });
 
-  test('user can change the number of events to display', () => {
+  test('value of numberOfEvents state changes when text input changes', () => {
     NumberOfEventsWrapper.find('#events-number').simulate('change', {target: { value: 10 },
     });
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(10);
